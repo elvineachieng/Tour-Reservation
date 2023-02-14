e91a977f7f404c28d7f64a635918414d749b341b
paullaster <paullasterokoth98@gmail.com>
2023-02-14 23:58:56 +0300

    commit files

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,5 +1,6 @@
 const acountIcon = document.querySelector('div.accounts > i');
 const sidebar = document.querySelector('aside.hide-sidebar');
 acountIcon.addEventListener('click', () =>{
-
+    sidebar.classList.remove('hide-sidebar');
+    sidebar.classList.add('sidebar');
 })
