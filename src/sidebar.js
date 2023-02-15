211704fa8453e9895a476d98e08416da1a08c98b
Elvine7067 <achiengelvine1@gmail.com>
2023-02-15 17:53:10 +0300

    fixed aside bar

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,6 +1,8 @@
 const acountIcon = document.querySelector('div.accounts > i');
 const sidebar = document.querySelector('aside.hide-sidebar');
 acountIcon.addEventListener('click', () =>{
-    sidebar.classList.remove('hide-sidebar');
-    sidebar.classList.add('sidebar');
+    // sidebar.classList.remove('hide-sidebar');
+    // sidebar.classList.add('sidebar');
+
+    sidebar.classList.toggle('aside-active');
 })
