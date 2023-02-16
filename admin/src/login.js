a8c767b490c4d7a27a69eff8f42b0d27dee440e3
paullaster <paullasterokoth98@gmail.com>
2023-02-17 01:18:23 +0300

    commit files

--- admin/src/login.js
+++ admin/src/login.js
@@ -17,6 +17,6 @@ form.addEventListener('submit', (event) =>{
             console.log(data.error);
             return;
         }
-        return window.location.href = '../dashboard.html';
+        return window.location.href = './dashboard.html';
     })
 });
