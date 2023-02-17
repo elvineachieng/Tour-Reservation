a27137990a284bb2ae0d0b74ceb4d71f5c0a2f50
paullaster <paullasterokoth98@gmail.com>
2023-02-17 10:25:32 +0300

    commit admin files

--- src/server/app.js
+++ src/server/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
-const { request } = require('http');
+const multer = require('multer');
 
 const app = express();
 
