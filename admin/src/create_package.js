9226e3908f0ff972afbdf8af4288df748b107853
paullaster <paullasterokoth98@gmail.com>
2023-02-17 11:30:08 +0300

    commit admin files

--- admin/src/create_package.js
+++ admin/src/create_package.js
@@ -4,12 +4,12 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
     console.log(formData.get('package_image'));
-    // fetch('http://localhost:3500/admin/create_packages', {
-    //     method: 'POST',
-    //     body: formData,
-    // })
-    // .then( response => response.json())
-    // .then( (responseData) =>{
-    //     console.log(responseData);
-    // })
+    fetch('http://localhost:3500/admin/create_packages', {
+        method: 'POST',
+        body: formData,
+    })
+    .then( response => response.json())
+    .then( (responseData) =>{
+        console.log(responseData);
+    })
 });
