a78dfc0ae37a0c78ddfc645c8a30368d5a80be99
paullaster <paullasterokoth98@gmail.com>
2023-02-17 16:51:37 +0300

    commit admin files

--- admin/src/view_packages.js
+++ admin/src/view_packages.js
@@ -2,9 +2,6 @@ const root = document.getElementById('view_packages');
 
 fetch('http:localhost:3500/admin/view_packages', {
     method: 'GET',
-    headers: {
-        'content-type': 'application/json',
-    }
 })
 .then ( (response) => response.json())
 .then( (responseData) => {
