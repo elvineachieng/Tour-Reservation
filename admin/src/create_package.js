1acda34d6ef56acb8b562961fb50f802f5711c61
paullaster <paullasterokoth98@gmail.com>
2023-02-17 12:41:15 +0300

    commit admin files

--- admin/src/create_package.js
+++ admin/src/create_package.js
@@ -3,7 +3,6 @@ const form = document.getElementById('create_package');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
-    console.log(formData.get('package_image'));
     fetch('http://localhost:3500/admin/create_packages', {
         method: 'POST',
         body: formData,
