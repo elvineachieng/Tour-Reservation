7aae69cca98ac70705ab2cd91c66b888bfcec329
paullaster <paullasterokoth98@gmail.com>
2023-02-17 12:47:53 +0300

    commit admin files

--- src/server/app.js
+++ src/server/app.js
@@ -103,6 +103,7 @@ admin.post('/create_packages',(req, res) => {
             });
         }
         const {...package} = req.body;
+        
         console.log(req.body);
         console.log(req.files);
     });
